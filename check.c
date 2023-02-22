6c0d98c1aabe811ed83075fd3a5cd2affa820466
Richard Israel Akpabio <richardakpabio0@gmail.com>
2023-02-22 04:38:36 -0800

    My first commit

---
+++ check.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(void)
+{
+	int fd;
+
+	char buffer[80];
+
+	char msg[50] = "hello world"; /*strings in file*/
+
+	fd = open("chk.txt", 0_RDWR | 0_CREAT);  /*created with rdwr access*/
+
+	printf("fd = %d", fd);   /*prints file descriptor*/
+
+	if (fd != -1)
+	{
+		printf("\n chk.txt opened with read write access\n");
+
+		write(fd, msg, sizeof(msg));   /*write to stdout*/
+
+		lseek(fd, 0, SEEK_SET);
+
+		read(fd, buffer, sizeof(msg));   /*reads from stdin*/
+
+		printf("\n %s was written to my file\n", buffer);
+
+		close(fd);
+	}
+
+	return (0);
+}
